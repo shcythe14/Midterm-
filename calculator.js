5e55ded44615d4779e18c18740144b3cd7e0a99b
Eric Mar Neri <32513259+shcythe14@users.noreply.github.com>
2019-01-31 16:09:44 +0800

    Add files via upload

---
+++ calculator.js
@@ -0,0 +1,25 @@
+function add(char) {
+	var display = document.getElementById('display');
+	display.value = display.value +char
+}
+function subtract(char) {
+	var display = document.getElementById('display');
+	display.value = display.value -char
+}
+function multiply(char) {
+	var display = document.getElementById('display');
+	display.value = display.value *char
+}
+function divide(char) {
+	var display = document.getElementById('display');
+	display.value = display.value /char
+}
+function calculate() {
+	var display = document.getElementById('display');
+	var result = eval(display.value);
+	document.getElementById('result').value = result
+}
+function reset() {
+	document.getElementById('display').value = "";
+	document.getElementById('result').value = "";
+}
